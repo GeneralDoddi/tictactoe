9b475983882b54a37a32e51420f5be6e1b82ca2a
Doddi <doddi@localhost.lan>
2014-11-28 15:35:55 +0000

    dockerbuild

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -4,6 +4,7 @@ echo Cleaning...
 rm -rf ./dist
 
 echo Building app
+npm install grunt
 grunt
 
 cp ./Dockerfile ./dist/
@@ -12,6 +13,6 @@ cd dist
 npm install --production
 
 echo Building docker image
-docker build -t generaldoddi/tictactoe .
+docker build -t generaldoddi/tictactoe ./dist
 
 echo "Done"
