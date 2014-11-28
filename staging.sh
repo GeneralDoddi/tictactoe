817e4a7f12fbd4cde09996b5a9ad2803b12d5605
Doddi <doddi@localhost.lan>
2014-11-28 15:38:55 +0000

    staging sh

---
+++ staging.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo Cleaning...
+rm -rf ./dist
+ln -s /usr/bin/nodejs /usr/bin/node
+echo "Installing grunt"
+npm install grunt
+echo "Installing bower"
+npm install bower
+echo "Npm install"
+npm install
+echo "Bower install"
+bower install
+echo "Running grunt"
+grunt
+echo "Done"
