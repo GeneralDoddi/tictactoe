218c569ce21831047c923dcead7e65f3d36ca498
generaldoddi <doddisti@gmail.com>
2014-11-30 18:28:32 +0000

    changes to staging

--- staging.sh
+++ staging.sh
@@ -5,7 +5,7 @@ ln -s /usr/bin/nodejs /usr/bin/node
 echo "Installing grunt"
 npm install grunt
 echo "Installing hooker"
-npme install hooker
+npm install hooker
 echo "Installing bower"
 npm install bower
 echo "Npm install"
