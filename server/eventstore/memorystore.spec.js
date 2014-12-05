f16561d8fec4024264bad46ddc5cd20f2c66110d
GeneralDoddi <doddisti@gmail.com>
2014-12-05 22:40:20 +0000

    test implementation

--- server/eventstore/memorystore.spec.js
+++ server/eventstore/memorystore.spec.js
@@ -6,6 +6,28 @@ var should = require('should');
 
 describe('In memory event store', function (){
 
+  var createGameEvent = {
+    id:"1337",
+    event: "GameCreated",
+    user:{
+      userName:"Doddi"
+    },
+    name:"GameOfLife",
+    timeStamp:"2014-01-01T03:06:00"
+
+  };
+
+  var joinGameEvent = {
+    id:"1338",
+    cmd: "JoinGame",
+    user:{
+      userName:"Gangsterinn"
+    },
+    name:"GameOfLife",
+    timeStamp:"2014-01-01T03:08:00"
+
+  };
+
   it('should return an empty array for unkown id', function(){
 
     var store = memorystore();
@@ -21,6 +43,23 @@ describe('In memory event store', function (){
 
     var store = memorystore();
 
+    store.storeEvents('1337',[createGameEvent]);
+
+    var loadedEvents = store.loadEvents('1337');
+
+    should(JSON.stringify(loadedEvents)).be.exactly(JSON.stringify([createGameEvent]));
+
+  });
+
+  it('should append stored events to previous stored events array', function(){
+    var store = memorystore();
+
+    store.storeEvents('1337',[createGameEvent,joinGameEvent]);
+
+    var loadedEvents = store.loadEvents('1337');
+
+    should(loadedEvents).eql([createGameEvent,joinGameEvent]);
+    should(loadedEvents).not.eql([joinGameEvent,createGameEvent]);
 
   });
 
